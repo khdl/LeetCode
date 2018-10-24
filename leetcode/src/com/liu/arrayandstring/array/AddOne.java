43a5a64c0327fb882ab20a49acd5cb9611cb9432
khdl <mykhdl123@gmail.com>
2018-10-25 00:38:23 +0800

    给定一个由整数组成的非空数组所表示的非负整数,在该数的基础上加一。

---
+++ leetcode/src/com/liu/arrayandstring/array/AddOne.java
@@ -0,0 +1,32 @@
+package com.liu.arrayandstring.array;
+
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Arrays;
+
+/**
+ * @ClassName: AddOne
+ * @Auther: yu
+ * @Date: 2018/10/24 23:30
+ * @Description:加一
+ */
+public class AddOne {
+    public static void main(String[] args){
+        int[] digits ={0};
+        int res[] = plusOne(digits);
+        System.out.println("");;
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for(int i=digits.length-1;i>=0;i--){
+            if(digits[i]<9){
+                digits[i] = digits[i]+1;
+                return digits;
+            }
+            digits[i]=0;
+        }
+        int[] res = new int[digits.length+1];
+        res[0] = 1;
+        return  res;
+    }
+}
