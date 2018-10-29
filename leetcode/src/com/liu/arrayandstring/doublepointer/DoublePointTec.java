dc0a4eef8d99f4662c3ed88fb0ea2eee731c20b3
khdl <mykhdl123@gmail.com>
2018-10-29 19:49:13 +0800

    反转字符串

---
+++ leetcode/src/com/liu/arrayandstring/doublepointer/DoublePointTec.java
@@ -0,0 +1,33 @@
+package com.liu.arrayandstring.doublepointer;
+
+/**
+ * @ClassName: DoublePointTec
+ * @Auther: yu
+ * @Date: 2018/10/29 19:33
+ * @Description: 双指针技巧
+ * 场景之一从两端向中间迭代数组。 
+ */
+public class DoublePointTec {
+
+	public static void main(String[] args) {
+	  int[] v ={1,2,3,4,5,6};
+	  int res[] = reverse(v,v.length);
+	  System.out.println(res);
+	}
+	public static int[] reverse(int[] v, int N) {
+	    int i = 0;
+	    int j = N - 1;
+	    while (i < j) {
+	        swap(v, i, j);  
+	        i++;
+	        j--;
+	    }
+	    return v;
+	}
+	private static void swap(int[] v, int i, int j) {
+		int tmp = v[i];
+		v[i] = v[j];
+		v[j] = tmp;
+		
+	}
+}
