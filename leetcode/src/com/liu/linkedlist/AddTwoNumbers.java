1c9103eab9481e398477e55fee01e2820a71cc82
khdl <mykhdl123@gmail.com>
2018-11-29 23:22:53 +0800

    扁平化多级双向链表

--- leetcode/src/com/liu/linkedlist/AddTwoNumbers.java
+++ leetcode/src/com/liu/linkedlist/AddTwoNumbers.java
@@ -13,7 +13,6 @@ public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode tmp = null;
         ListNode result = null;
-
         int carry = 0;
 
         while(l1 != null || l2 != null || carry != 0){
