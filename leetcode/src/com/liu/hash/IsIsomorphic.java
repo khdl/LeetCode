65eb8f032f3449747127c663f7002c37162631c3
khdl <mykhdl123@gmail.com>
2018-12-05 21:08:47 +0800

    哈希集合

---
+++ leetcode/src/com/liu/hash/IsIsomorphic.java
@@ -0,0 +1,37 @@
+package com.liu.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName: IsIsomorphic
+ * @Auther: yu
+ * @Date: 2018/12/5 20:12
+ * @Description:同构字符串
+ * 给定两个字符串 s 和 t，判断它们是否是同构的
+ */
+public class IsIsomorphic {
+    public static void main(String[] args){
+       String a = "egg";
+       String b = "add";
+       isIsomorphic(a,b);
+    }
+    public static  boolean isIsomorphic(String s, String t) {
+        char[] ss = s.toCharArray();
+        char[] tt = t.toCharArray();
+        Map<Character,Character> map = new HashMap<>();
+        for (int i = 0; i <ss.length ; i++) {
+            if(!map.containsKey(ss[i]) ){
+                if(!map.containsValue(tt[i])){
+                    map.put(ss[i],tt[i]);
+                }else{
+                    return false;
+                }
+            }
+             if(!map.get(ss[i]).equals(tt[i])){
+                 return  false;
+             }
+        }
+        return true;
+    }
+}
---
+++ leetcode/src/com/liu/hash/TwoSum.java
@@ -0,0 +1,28 @@
+package com.liu.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @ClassName: TwoSum
+ * @Auther: yu
+ * @Date: 2018/12/5 20:00
+ * @Description:两数之和
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的 两个 整数
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ */
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0 ; i < nums.length; i++){
+            if(map.containsKey(target-nums[i])){
+                res[0] = i;
+                res[1] = map.get(target - nums[i]);
+                break;
+            }
+            map.put(nums[i],i);
+        }
+        return  res;
+    }
+}
