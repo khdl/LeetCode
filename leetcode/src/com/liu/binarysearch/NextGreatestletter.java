0e521497d66a18f583e06c97c4e3fac0eed38ac2
khdl <mykhdl123@gmail.com>
2019-01-11 19:04:28 +0800

    练习

---
+++ leetcode/src/com/liu/binarysearch/NextGreatestletter.java
@@ -0,0 +1,28 @@
+package com.liu.binarysearch;
+
+/**
+ * @ClassName: NextGreatestletter
+ * @Auther: yu
+ * @Date: 2019/1/11 16:49
+ * @Description:  寻找比目标字母大的最小字母
+ * 给定一个只包含小写字母的有序数组letters 和一个目标字母 target，寻找有序数组里面比目标字母大的最小字母。
+ */
+public class NextGreatestletter {
+    /**
+     * 这种方法时间复杂度为O(n),可用二分提高效率
+     * @param letters
+     * @param target
+     * @return
+     */
+    public char nextGreatestLetter(char[] letters, char target) {
+        if(target>=letters[letters.length-1]) {
+            return letters[0];
+        }
+        for(int i=0;i<letters.length;i++) {
+            if(letters[i]>target) {
+                return letters[i];
+            }
+        }
+        return letters[0];
+    }
+}
