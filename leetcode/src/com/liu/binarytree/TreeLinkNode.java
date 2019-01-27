b4fc756298e2246d0cfd4888527276c327fdf54b
khdl <mykhdl123@gmail.com>
2019-01-27 18:12:18 +0800

    每个节点的右向指针

---
+++ leetcode/src/com/liu/binarytree/TreeLinkNode.java
@@ -0,0 +1,13 @@
+package com.liu.binarytree;
+
+/**
+ * @ClassName: TreeLinkNode
+ * @Auther: yu
+ * @Date: 2019/1/27 06:38
+ * @Description:
+ */
+public class TreeLinkNode {
+     int val;
+     TreeLinkNode left, right, next;
+     TreeLinkNode(int x) { val = x; }
+}
