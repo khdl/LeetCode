43bfcff840ef0f17f2d66eb8e84ff2e69f7da276
khdl <mykhdl123@gmail.com>
2019-02-17 19:57:25 +0800

    N叉树的遍历

---
+++ leetcode/src/com/liu/narytree/Levelorder.java
@@ -0,0 +1,30 @@
+package com.liu.narytree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName: Levelorder
+ * @Auther: yu
+ * @Date: 2019/2/17 19:59
+ * @Description: N叉树的层序遍历
+ */
+public class Levelorder {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(root == null){
+            return res;
+        }
+        fillRes(root, 0, res);
+        return res;
+    }
+    public void fillRes(Node root, int level, List<List<Integer>> res) {
+        if(res.size() < level + 1){
+            res.add(new ArrayList<Integer>());
+        }
+        res.get(level).add(root.val);
+        for (Node n : root.children) {
+            fillRes(n, level + 1, res);
+        }
+    }
+}
---
+++ leetcode/src/com/liu/narytree/Preorder.java
@@ -0,0 +1,30 @@
+package com.liu.narytree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @ClassName: preorder
+ * @Auther: yu
+ * @Date: 2019/2/17 19:35
+ * @Description: N-ary Tree Preorder Traversal
+ * 给定一个 N 叉树，返回其节点值的前序遍历。
+ */
+public class Preorder {
+    public List<Integer> preorder(Node root) {
+        List<Integer> res = new LinkedList<>();
+        prehel(root,res);
+        return res;
+
+    }
+    public void prehel(Node root,List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        List<Node> child = root.children;
+        res.add(root.val);
+        for (Node ele : child) {
+            prehel(ele, res);
+        }
+    }
+}
