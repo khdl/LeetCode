a7f0b47d0e45b832b320be71d2cd3934f1a0f9f6
khdl <mykhdl123@gmail.com>
2019-02-23 20:46:06 +0800

    Maximum Depth of N-ary Tree

---
+++ leetcode/src/com/liu/narytree/MaxDepth.java
@@ -0,0 +1,29 @@
+package com.liu.narytree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName: MaxDepth
+ * @Auther: yu
+ * @Date: 2019/2/23 20:39
+ * @Description: Maximum Depth of N-ary Tree
+ */
+public class MaxDepth {
+    public int maxDepth(Node root) {
+        if(root == null){
+            return 0;
+        }
+        List<Integer> temp = new ArrayList<>();
+        for(int i=0;i<root.children.size();i++){
+            temp.add(maxDepth(root.children.get(i)));
+        }
+        int max = 0;
+        for(int i=0;i<temp.size();i++){
+            if(temp.get(i)>max) {
+                max = temp.get(i);
+            }
+        }
+        return max+1;
+    }
+}
